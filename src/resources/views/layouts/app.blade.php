31d751e02507beba3d4bcca12d953a535892cb77
aruma-joishi <aru-jo@docmo.ne.jp>
2023-12-09 16:33:53 +0900

    ログイン、新規作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,6 +13,18 @@
 <body>
   <header class="header">
     <div class="header__inner">
+      @if (Auth::check())
+  <li class="header-nav__item">
+    <a class="header-nav__link" href="/mypage">マイページ</a>
+  </li>
+  <li class="header-nav__item">
+    <form action="/logout" method="post">
+      @csrf
+      <button class="header-nav__button">ログアウト</button>
+    </form>
+  </li>
+ @endif
+</ul>
       <a class="header__logo" href="/login">
         Login 
       </a>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
+use App\Http\Controllers\AuthController;
+
+//問い合わせ
 Route::get('/', [ContactController::class,'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
 Route::post('/contacts', [ContactController::class, 'store']);
+
+//ログイン、アカウント作成
+Route::middleware('auth')->group(function () {
+  Route::get('/admin', [AuthController::class, 'index']);
+});
